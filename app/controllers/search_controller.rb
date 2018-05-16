b9867e96c5e1929ab85d09979b78d6621a32c6c3
alexcutschall <alexander.cutschall@gmail.com>
2018-05-16 09:55:38 -0600

    new api key

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,10 @@
 class SearchController < ApplicationController
   def index
-    conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=#{ENV['API_KEY']}&format=JSON")
+    conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=0TWVyL6HFkXC44dCq7wHlpElOLWkpyzJnkHiekhS&format=JSON")
 
-    binding.pry
     raw_search = conn.get '/nearest'
 
     response = JSON.parse(raw_search)
+    binding.pry
   end
 end
