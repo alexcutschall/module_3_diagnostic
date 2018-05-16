4930722a6e81debb7d1b9a71994c40bb03473768
alexcutschall <alexander.cutschall@gmail.com>
2018-05-16 10:30:55 -0600

    parsing out data

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,12 +3,12 @@ class SearchController < ApplicationController
     zip_code = params["q"]
     conn = Faraday.new(url: "https://developer.nrel.gov")
 
-    response = conn.get "/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['API_KEY']}&location=#{zip_code}&limit=10"
+    response = conn.get "/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['API_KEY']}&location=#{zip_code}&LIMIT=10&fuel_type=LPG, ELEC"
 
-    stations = JSON.parse(response.body)
-    stations.each do |station|
+    stations = JSON.parse(response.body, symbolize_names: true)
+    binding.pry
+    stations["fuel_stations"].each do |station|
       Station.new(station)
     end
-    binding.pry
   end
 end
