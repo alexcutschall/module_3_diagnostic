53893ed34cdd5b3740f3c30b861d68e927c9ab15
alexcutschall <alexander.cutschall@gmail.com>
2018-05-16 10:59:24 -0600

    adding in extra expects

--- spec/features/user_can_search_stations_spec.rb
+++ spec/features/user_can_search_stations_spec.rb
@@ -17,13 +17,24 @@ describe 'as a user' do
         # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
         expect(page).to have_content("ELEC")
         expect(page).to_not have_content("BD")
+        expect(page).to_not have_content("E85")
+        expect(page).to_not have_content("CNG")
+        expect(page).to_not have_content("HY")
+        expect(page).to_not have_content("LNG")
         # And the stations should be limited to Electric and Propane
         first(".station") do
-          expect(page).to include("Name")
-          expect(page).to include("Address")
-          expect(page).to include("Fuel Types")
-          expect(page).to include("Distance")
-          expect(page).to include("Access Times")
+          expect(page).to have_content("Name")
+          expect(page).to have_content("Address")
+          expect(page).to have_content("Fuel Type")
+          expect(page).to have_content("Distance")
+          expect(page).to have_content("Access Times")
+        end
+        all(".station").last do
+          expect(page).to have_content("Name")
+          expect(page).to have_content("Address")
+          expect(page).to have_content("Fuel Type")
+          expect(page).to have_content("Distance")
+          expect(page).to have_content("Access Times")
         end
         # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
       end
