44ae21b9f4c7a1b47626d46c069649869f84134a
alexcutschall <alexander.cutschall@gmail.com>
2018-05-16 10:36:27 -0600

    finished creating a station PORO

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,9 +6,9 @@ class SearchController < ApplicationController
     response = conn.get "/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['API_KEY']}&location=#{zip_code}&LIMIT=10&fuel_type=LPG, ELEC"
 
     stations = JSON.parse(response.body, symbolize_names: true)
-    binding.pry
-    stations["fuel_stations"].each do |station|
+    stations[:fuel_stations].each do |station|
       Station.new(station)
     end
+    binding.pry
   end
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -1,4 +1,11 @@
 class Station
   def initialize(attributes)
+    @name = attributes[:station_name]
+    @address = attributes[:street_address]
+    @city = attributes[:city]
+    @state = attributes[:state]
+    @fuel_type = attributes[:fuel_type_code]
+    @distance = attributes[:distance]
+    @access_times = attributes[:access_days_time]
   end
 end
